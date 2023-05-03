ee3e30b79a942dc6a17e95290170fabfdb73e7e5
Victor Piles <victorpilesdelgado@gmail.com>
2023-05-03 10:12:26 +0200

    Creació del projecte backend
    Signed-off-by: Victor Piles <victorpilesdelgado@gmail.com>

---
+++ src/main/java/org/victorpiles/escacs/EscacsApplication.java
@@ -0,0 +1,12 @@
+package org.victorpiles.escacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscacsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscacsApplication.class, args);
+    }
+}
