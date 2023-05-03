41921bdb4006c4e5a774edc026d1534bb5ce6afd
Victor Piles <victorpilesdelgado@gmail.com>
2023-05-03 10:13:58 +0200

    Excepcions específiques per al login i registre d'usuaris
    Signed-off-by: Victor Piles <victorpilesdelgado@gmail.com>

---
+++ src/main/java/org/victorpiles/escacs/api/exception/BadCredentialsException.java
@@ -0,0 +1,18 @@
+package org.victorpiles.escacs.api.exception;
+
+import org.victorpiles.escacs.api.user.User;
+import org.victorpiles.escacs.api.user.UserService;
+
+/**
+ * Tipus d'{@link IllegalStateException excepció} utilitzada en la comprovació de credencials de l'{@link User usuari}.
+ *
+ * @author Víctor Piles
+ * @version 1.0
+ * @see UserService#login(String, String)
+ */
+public class BadCredentialsException extends IllegalStateException {
+
+    public BadCredentialsException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/org/victorpiles/escacs/api/exception/EmailNotFoundException.java
@@ -0,0 +1,17 @@
+package org.victorpiles.escacs.api.exception;
+
+import org.victorpiles.escacs.api.user.User;
+
+/**
+ * Tipus d'{@link IllegalStateException excepció} utilitzada quan el {@link User#getEmail() correu} no existeix a la
+ * base de dades.
+ *
+ * @author Víctor Piles
+ * @version 1.0
+ */
+public class EmailNotFoundException extends IllegalStateException {
+
+    public EmailNotFoundException(String s) {
+        super(s);
+    }
+}
