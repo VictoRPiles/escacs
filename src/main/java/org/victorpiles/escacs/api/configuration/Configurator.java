ab632747c790e636e57841503be8c268bdf73495
Victor Piles <victorpilesdelgado@gmail.com>
2023-05-08 13:09:57 +0200

    Afegint requests al configurador
    Signed-off-by: Victor Piles <victorpilesdelgado@gmail.com>

--- src/main/java/org/victorpiles/escacs/api/configuration/Configurator.java
+++ src/main/java/org/victorpiles/escacs/api/configuration/Configurator.java
@@ -3,14 +3,13 @@ package org.victorpiles.escacs.api.configuration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.victorpiles.escacs.api.move.Move;
+import org.victorpiles.escacs.api.gamerequest.GameRequestService;
 import org.victorpiles.escacs.api.move.MoveService;
 import org.victorpiles.escacs.api.user.User;
 import org.victorpiles.escacs.api.user.UserService;
 
 /**
- * Configuració inicial. {@link UserService#register(User) Registra} un {@link User usuari} per defecte amb dos
- * {@link Move moviments}.
+ * Configuració inicial.
  *
  * @author Víctor Piles
  * @version 1.0
@@ -19,17 +18,29 @@ import org.victorpiles.escacs.api.user.UserService;
 public class Configurator {
 
     @Bean
-    CommandLineRunner commandLineRunner(UserService userService, MoveService moveService) {
+    CommandLineRunner commandLineRunner(
+            UserService userService,
+            MoveService moveService,
+            GameRequestService gameRequestService
+    ) {
         return args -> {
-            User user = new User(
+            User administrator = new User(
                     "Administrator",
                     "admin@escacs.org",
                     "admin@escacs.org"
             );
+            User user = new User(
+                    "User",
+                    "user@escacs.org",
+                    "user@escacs.org"
+            );
+            userService.register(administrator);
             userService.register(user);
 
             moveService.execute("f4", "Administrator");
             moveService.execute("exf4", "Administrator");
+
+            gameRequestService.send("Administrator", "User");
         };
     }
 }
