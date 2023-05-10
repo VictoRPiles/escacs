354dbb02cb09172f5d1a16c072c49ab3a82fd1e0
Victor Piles <victorpilesdelgado@gmail.com>
2023-05-10 14:04:31 +0200

    Tests per a NotationConverter
    Signed-off-by: Victor Piles <victorpilesdelgado@gmail.com>

---
+++ src/test/java/org/victorpiles/escacs/engine/util/NotationConverterTest.java
@@ -0,0 +1,22 @@
+package org.victorpiles.escacs.engine.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NotationConverterTest {
+
+    @Test
+    void notationToIndex() {
+        assertEquals(0, NotationConverter.notationToIndex("a8"));
+        assertEquals(1, NotationConverter.notationToIndex("b8"));
+        assertEquals(63, NotationConverter.notationToIndex("h1"));
+    }
+
+    @Test
+    void indexToNotation() {
+        assertEquals("a8", NotationConverter.indexToNotation(0));
+        assertEquals("b8", NotationConverter.indexToNotation(1));
+        assertEquals("h1", NotationConverter.indexToNotation(63));
+    }
+}
