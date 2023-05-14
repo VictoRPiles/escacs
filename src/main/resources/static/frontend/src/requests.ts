c35e1d7d23d7b00ffff54c8d2bbdf21c24b31cd2
Victor Piles <victorpilesdelgado@gmail.com>
2023-05-14 18:39:45 +0200

    Funcions per a fer peticions HTTP
    Signed-off-by: Victor Piles <victorpilesdelgado@gmail.com>

---
+++ src/main/resources/static/frontend/src/requests.ts
@@ -0,0 +1,38 @@
+const HTTP_GET = "GET";
+const HTTP_POST = "POST";
+const HTTP_PUT = "PUT";
+
+async function fetchWithMethod<T>(httpMethod: string, url: string, parameters: Map<string, string> | null) {
+    if (parameters !== null) {
+        url = url.concat(toUrlParameters(parameters));
+        console.log(url);
+    }
+    let response = await fetch(url, {method: httpMethod});
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return await response.json() as Promise<T>;
+}
+
+async function get<T>(url: string, parameters: Map<string, string> | null): Promise<T> {
+    return await fetchWithMethod(HTTP_GET, url, parameters);
+}
+
+async function post<T>(url: string, parameters: Map<string, string> | null): Promise<T> {
+    return await fetchWithMethod(HTTP_POST, url, parameters);
+}
+
+async function put<T>(url: string, parameters: Map<string, string> | null): Promise<T> {
+    return await fetchWithMethod(HTTP_PUT, url, parameters);
+}
+
+function toUrlParameters(parameters: Map<string, string>) {
+    let urlParameters: string = "";
+    urlParameters = urlParameters.concat("?");
+    for (let entry of parameters.entries()) {
+        let key = entry[0];
+        let value = entry[1];
+        urlParameters = urlParameters.concat(key).concat("=").concat(value).concat("&");
+    }
+    return urlParameters;
+}
